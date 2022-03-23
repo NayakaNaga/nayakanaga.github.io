e08bcd33df83a8a05f5fdadb536564a2b2e62f52
NayakaNaga <nayakanaga@gmail.com>
2022-03-23 12:02:52 -0300

    first commit

---
+++ games.js
@@ -0,0 +1,1250 @@
+var data = [];
+data.push(
+	{
+		"name": "Princess Peach",
+		"game": "Super Mario Bros",
+		"type": "👈",
+		"system": "snes",
+	},
+	{
+		"name": "Princess Peach",
+		"game": "Super Mario Lost Levels",
+		"type": "👉",
+		"system": "snes"
+	},
+	{
+		"name": "Princess Peach",
+		"game": "Super Mario 2",
+		"type": "💧",
+		"system": "snes"
+	},
+	{
+		"name": "Princess Peach",
+		"game": "Super Mario 3",
+		"type": "🌞",
+		"system": "snes"
+	},	
+	{
+		"name": "",
+		"game": "F-Zero",
+		"type": "💀",
+		"system": "snes"
+	},	
+	{
+		"name": "Princess Peach",
+		"game": "Super Mario Wrold",
+		"type": "📺",
+		"system": "snes"
+	},
+	{
+		"name": "Blonde",
+		"game": "Hit the Ice",
+		"type": "👈",
+		"system": "snes"
+	},
+	{
+		"name": "",
+		"game": "Top Gear",
+		"type": "💀",
+		"system": "snes"
+	},
+	{
+		"name": "Princess Peach",
+		"game": "Super Mario Kart",
+		"type": "💭",
+		"system": "snes"
+	},
+	{
+		"name": "Sonya Blade",
+		"game": "Mortal Kombat",
+		"type": "👈",
+		"system": "snes"
+	},
+	{
+		"name": "",
+		"game": "Super Pang",
+		"type": "💀",
+		"system": "snes"
+	},
+	{
+		"name": "Mao Mao",
+		"game": "Sonic Wings",
+		"type": "👈",
+		"system": "snes"
+	},
+	{
+		"name": "",
+		"game": "Super Bomberman",
+		"type": "💀",
+		"system": "snes"
+	},
+	{
+		"name": "Cammy White",
+		"game": "Street Fighter II",
+		"type": "👈",
+		"system": "snes"
+	},
+	{
+		"name": "",
+		"game": "Goof Troop",
+		"type": "💀",
+		"system": "snes"
+	},	
+	{
+		"name": "Jasmine",
+		"game": "Aladdin",
+		"type": "👈",
+		"system": "snes"
+	},
+	{
+		"name": "Barbie",
+		"game": "Barbie",
+		"type": "👈",
+		"system": "snes"
+	},
+	{
+		"name": "",
+		"game": "Beethoven 2nd",
+		"type": "💀",
+		"system": "snes"
+	},		
+	{
+		"name": "Pretty Bomber",
+		"game": "Super Bomberman 2",
+		"type": "👈",
+		"system": "snes"
+	},
+	{
+		"name": "",
+		"game": "Puzzle Bubble",
+		"type": "💀",
+		"system": "snes"
+	},	
+	{
+		"name": "Pretty Bomber",
+		"game": "Super Bomberman 3",
+		"type": "👉",
+		"system": "snes"
+	},
+	{
+		"name": "",
+		"game": "Yoshis land",
+		"type": "💀",
+		"system": "snes"
+	},
+	{
+		"name": "",
+		"game": "Tetris Attack",
+		"type": "💀",
+		"system": "snes"
+	},
+	{
+		"name": "Kitana",
+		"game": "Mortal Kombat 3 Ultimate",
+		"type": "👈",
+		"system": "snes"
+	},
+	{
+		"name": "Striper",
+		"type": "👌",
+		"system": "pc",
+		"game": "Duke Nukem 3D"	
+	},
+	{
+		"name": "Lady Bomber",
+		"game": "Super Bomberman 4",
+		"type": "👈",
+		"system": "snes"
+	},
+	{
+		"name": "",
+		"game": "Mr Do",
+		"type": "💀",
+		"system": "snes"
+	},
+	{
+		"name": "",
+		"game": "Super Bomberman 5",
+		"type": "💀",
+		"system": "snes"
+	},	
+	{
+		"name": "Gina Cross",
+		"type": "🍌",
+		"system": "pc",
+		"game": "Half-live"
+	},
+	{
+		"name": "Black Ops",
+		"type": "♿",
+		"system": "pc",
+		"game": "Half-live Opposing force"
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "Pacific Warriors"
+	},	
+	{
+		"name": "Black Cat",
+		"type": "💧",
+		"system": "pc",
+		"game": "Amazing Spider-man",
+	},
+		{
+		"name": "Princess Peach",
+		"type": "👌",
+		"system": "n64",
+		"game": "Super Mario 64"
+	},	
+	{
+		"name": "Tanya",
+		"type": "📺",
+		"system": "n64",
+		"game": "Mortal Kombat 4",
+	},
+	{
+		"name": "Gina Cross",
+		"type": "♿",
+		"system": "pc",
+		"game": "Half-live Blue shift"
+	},
+	{
+		"name": "Mona Sax",
+		"type": "♿",
+		"system": "pc",
+		"game": "Max Payne"
+	},	
+	{
+		"name": "Michelle",
+		"type": "♿",
+		"system": "pc",
+		"game": "Mafia"
+	},	
+	{
+		"name": "Candy Suxxx",
+		"type": "🚶",
+		"system": "pc",
+		"game": "GTA Vice City",
+	},
+	{
+		"name": "Mona Sax",
+		"type": "🛀",
+		"system": "pc",
+		"game": "Max Payne 2"
+	},
+	{
+		"name": "Melissa",
+		"type": "👌",
+		"system": "pc",
+		"game": "NFS Underground"
+	},
+	{
+		"name": "Valerie Constantine",
+		"type": "💭",
+		"system": "pc",
+		"game": "FarCry",
+	},	
+	{
+		"name": "School Girl",
+		"type": "🌜",
+		"system": "pc",
+		"game": "CS Condition Zero"	
+	},	
+	{
+		"name": "Kendl Johnson",
+		"type": "🚶",
+		"system": "pc",
+		"game": "GTA San Andreas",
+	},
+	{
+		"name": "Rachel Teller",
+		"type": "👌",
+		"system": "pc",
+		"game": "NFS Underground 2"
+	},	
+	{
+		"name": "Jeanette Voerman",
+		"type": "🍌",
+		"system": "pc",
+		"game": "Vampire the mascarade bloodlines"
+	},
+	{
+		"name": "Alyx Vance",
+		"type": "🍌",
+		"system": "pc",
+		"game": "Half-live 2"
+	},	
+	{
+		"name": "Jin Sun-Kwon",
+		"type": "🚶",
+		"system": "pc",
+		"game": "FEAR",
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "Sniper Elite"
+	},
+	{
+		"name": "Mia Townsend",
+		"type": "🍌",
+		"system": "pc",
+		"game": "NFS Most Wanted"
+	},
+	{
+		"name": "Molly Ferguson",
+		"type": "🚶",
+		"system": "pc",
+		"game": "Call of Juarez"
+	},
+	{
+		"name": "Amanda Evert",
+		"type": "🛀",
+		"system": "pc",
+		"game": "Tomb Raider Lengend"
+	},
+	{
+		"name": "Judith Mossman",
+		"type": "♿",
+		"system": "pc",
+		"game": "Half-live 2 episode 1"
+	},
+	{
+		"name": "Nikki",
+		"type": "🍌",
+		"system": "pc",
+		"game": "NFS Carbon"
+	},	
+	{
+		"name": "Lara Croft",
+		"type": "🛀",
+		"system": "pc",
+		"game": "Tomb Raider Anniversary"
+	},
+	{
+		"name": "Brigid Tenenbaum",
+		"type": "📺",
+		"system": "pc",
+		"game": "BioShock",
+	},
+	{
+		"name": "Alyx Vance",
+		"type": "♿",
+		"system": "pc",
+		"game": "Half-live 2 episode 2"
+	},
+	{
+		"name": "Chell",
+		"type": "🍌",
+		"system": "pc",
+		"game": "Portal"
+	},
+	{
+		"name": "Triss Merigold",
+		"type": "👌",
+		"system": "pc",
+		"game": "The Witcher"
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "COD MW",
+	},
+	{
+		"name": "Lucy Stillman",
+		"type": "💧",
+		"system": "pc",
+		"game": "AC",
+	},
+	{
+		"name": "Helena Rosenthal",
+		"type": "🍌",
+		"system": "pc",
+		"game": "Crysis"
+	},
+	{
+		"name": "Elena Fisher",
+		"type": "👈",
+		"system": "ps3",
+		"game": "Uncharted",
+	},
+	{
+		"name": "Ashley Williams",
+		"type": "👌",
+		"system": "pc",
+		"game": "Mass Effect"
+	},
+	{
+		"name": "Karen Daniels",
+		"type": "🚶",
+		"system": "pc",
+		"game": "GTA 4",
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "SGW"
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "Crysis Warhead"	
+	},
+	{
+		"name": "Juri",
+		"type": "🌜",
+		"system": "pc",
+		"game": "Street Fighter IV"
+	},
+	{
+		"name": "Kendra Daniels",
+		"type": "📺",
+		"system": "pc",
+		"game": "Dead Space",
+	},
+	{
+		"name": "Nasreen Davar",
+		"type": "💭",
+		"system": "pc",
+		"game": "FarCry 2",
+	},	
+	{
+		"name": "Amata Almodovar",
+		"type": "💭",
+		"system": "pc",
+		"game": "Fallout 3",
+	},
+	{
+		"name": "Faith Connors",
+		"type": "🛀",
+		"system": "pc",
+		"game": "Mirross Edge"
+	},
+	{
+		"name": "Cristina Vespucci",
+		"type": "💧",
+		"system": "pc",
+		"game": "AC 2",
+	},
+	{
+		"name": "Jacqueline Natla ",
+		"type": "🛀",
+		"system": "pc",
+		"game": "Tomb Raider Underworld"
+	},
+	{
+		"name": "Chase Linh",
+		"type": "🍌",
+		"system": "pc",
+		"game": "NFS Undercover"
+	},
+	{
+		"name": "Keira Stokes",
+		"type": "🚶",
+		"system": "pc",
+		"game": "FEAR 2",
+	},	
+	{
+		"name": "Marisa",
+		"type": "♿",
+		"system": "pc",
+		"game": "Call of Juarez bound in blood"
+	},
+	{
+		"name": "Poison Ivy",
+		"type": "🌞",
+		"system": "pc",
+		"game": "Batman Arkham Asylum",
+	},
+	{
+		"name": "Rika Raja",
+		"type": "👈",
+		"system": "ps3",
+		"game": "Uncharted 2",
+	},	
+	{
+		"name": "Lilith",
+		"type": "📺",
+		"system": "pc",
+		"game": "Borderlands",
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "COD MW 2",
+	},
+	{
+		"name": "Rochelle",
+		"type": "🚶",
+		"system": "pc",
+		"game": "Left 4 Dead 2",
+	},
+	{
+		"name": "Uriel",
+		"game": "Darksiders",
+		"type": "💧",
+		"system": "pc",
+	},
+	{
+		"name": "Miranda Lawson",
+		"type": "👌",
+		"system": "pc",
+		"game": "Mass Effect 2"
+	},
+	{
+		"name": "Eleanor Lamb \"Big Sister\"",
+		"type": "📺",
+		"system": "pc",
+		"game": "BioShock 2",
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "BF Bad Company 2",
+	},
+	{
+		"name": "Nikki",
+		"type": "🛀",
+		"system": "pc",
+		"game": "Metro 2033"
+	},
+	{
+		"name": "Bonnie MacFarlane",
+		"type": "👉",
+		"system": "ps3",
+		"game": "RDR",
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "Limbo",
+	},
+	{
+		"name": "Francesca Scaletta",
+		"type": "♿",
+		"system": "pc",
+		"game": "Mafia II"
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "Medal of Honor"
+	},
+	{
+		"name": "Rose of Sharon Cassidy",
+		"type": "💭",
+		"system": "pc",
+		"game": "Fallout New Vegas",
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "COD BlackOps",
+	},
+	{
+		"name": "Claudia Auditore",
+		"type": "💧",
+		"system": "pc",
+		"game": "AC Brotherhood",
+	},
+	{
+		"name": "Ellie Langford",
+		"type": "📺",
+		"system": "pc",
+		"game": "Dead Space 2",
+	},
+	{
+		"name": "Tara Strickland",
+		"type": "🍌",
+		"system": "pc",
+		"game": "Crysis 2"
+	},
+	{
+		"name": "Skarlet",
+		"type": "👈",
+		"system": "ps3",
+		"game": "Mortal Kombat 9",
+	},
+	{
+		"name": "Kitty Pousoix",
+		"type": "🍌",
+		"system": "pc",
+		"game": "Duke Nukem Forever"	
+	},
+	{
+		"name": "Sìle de Tansarville",
+		"type": "👌",
+		"system": "pc",
+		"game": "The Witcher 2"
+	},
+	{
+		"name": "Chell",
+		"type": "🛀",
+		"system": "pc",
+		"game": "Portal 2"
+	},
+	{
+		"name": "Alma Wade",
+		"type": "🚶",
+		"system": "pc",
+		"game": "FEAR 3",
+	},
+	{
+		"name": "Talia al Ghul",
+		"type": "🌞",
+		"system": "pc",
+		"game": "Batman Arkham City",
+	},
+	{
+		"name": "Jennifer Hawkins",
+		"type": "🌞",
+		"system": "pc",
+		"game": "BF 3",
+	},
+	{
+		"name": "Chloe Frazer",
+		"type": "👉",
+		"system": "ps3",
+		"game": "Uncharted 3",
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "COD MW 3",
+	},
+	{
+		"name": "Lydia",
+		"type": "💭",
+		"system": "pc",
+		"game": "Skyrim",
+	},
+	{
+		"name": "Sofia Sartor",
+		"type": "💧",
+		"system": "pc",
+		"game": "AC Revelations",
+	},
+	{
+		"name": "Shaundi",
+		"type": "🛀",
+		"system": "pc",
+		"game": "Saints Row 3"
+	},
+	{
+		"name": "Alex",
+		"type": "💭",
+		"system": "pc",
+		"game": "Minecraft"	
+	},
+	{
+		"name": "Jenny Romano",
+		"type": "📺",
+		"system": "pc",
+		"game": "The Darkness II",
+	},
+	{
+		"name": "Liara T'Soni",
+		"type": "👌",
+		"system": "pc",
+		"game": "Mass Effect 3"
+	},
+	{
+		"name": "The Traveler",
+		"type": "👉",
+		"system": "ps3",
+		"game": "Journey"
+	},	
+	{
+		"name": "Carley",
+		"type": "🌜",
+		"system": "pc",
+		"game": "TWD"
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "Sniper Elite V2"
+	},	
+	{
+		"name": "Fabiana Branco",
+		"type": "🛀",
+		"system": "pc",
+		"game": "Max Payne 3"
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "Deadlight",
+	},
+	{
+		"name": "Alya",
+		"type": "📺",
+		"system": "pc",
+		"game": "Darksiders 2",
+	},
+	{
+		"name": "Sandra",
+		"type": "🌜",
+		"system": "pc",
+		"game": "Sleeping Dogs"
+	},
+	{
+		"name": "Maya",
+		"type": "📺",
+		"system": "pc",
+		"game": "Borderlands 2",
+	},
+	{
+		"name": "Jessamine Kaldwin",
+		"type": "📺",
+		"system": "pc",
+		"game": "Dishonored",
+	},
+	{
+		"name": "Citra Talugmai",
+		"type": "💭",
+		"system": "pc",
+		"game": "FarCry 3",
+	},
+	{
+		"name": "Kaniehti:io",
+		"type": "💧",
+		"system": "pc",
+		"game": "AC 3",
+	},
+	{
+		"name": "Samantha Nishimura ",
+		"type": "🌜",
+		"system": "pc",
+		"game": "Tomb Raider"
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "SGW 2"
+	},	
+	{
+		"name": "Elizabeth Comstock",
+		"type": "📺",
+		"system": "pc",
+		"game": "BioShock infinite",
+	},
+	{
+		"name": "Molly",
+		"type": "♿",
+		"system": "pc",
+		"game": "Call of Juarez gunslinger"
+	},
+	{
+		"name": "Anna Miller",
+		"type": "🛀",
+		"system": "pc",
+		"game": "Metro 2033 last light"
+	},
+	{
+		"name": "Dr. Elizabeth",
+		"type": "💭",
+		"system": "pc",
+		"game": "FarCry 3 Blood Dragon",
+	},
+	{
+		"name": "Tess",
+		"type": "👉",
+		"system": "ps3",
+		"game": "The Last of Us",
+	},
+	{
+		"name": "Tracey De Santa",
+		"type": "🚶",
+		"system": "ps3",
+		"game": "GTA V",
+	},
+	{
+		"name": "Anna",
+		"type": "💭",
+		"system": "pc",
+		"game": "Valiant Hearts"	
+	},
+	{
+		"name": "Korka Tetirdottir",
+		"type": "♿",
+		"system": "pc",
+		"game": "Puzzle Agent 2"	
+	},
+	{
+		"name": "Ameonna",
+		"type": "👈",
+		"system": "pc",
+		"game": "Shadow Warrior"	
+	},
+	{
+		"name": "Snow White",
+		"type": "👌",
+		"system": "pc",
+		"game": "The wolf amoung us"
+	},
+	{
+		"name": "Kinzie Kensington",
+		"type": "🛀",
+		"system": "pc",
+		"game": "Saints Row 4"
+	},
+	{
+		"name": "Harley Quinn",
+		"type": "🌞",
+		"system": "pc",
+		"game": "Batman Arkham Origins",
+	},
+	{
+		"name": "Anne Bonny",
+		"type": "💧",
+		"system": "pc",
+		"game": "AC Black Flag",
+	},
+	{
+		"name": "Huang Shuyi",
+		"type": "🌞",
+		"system": "pc",
+		"game": "BF 4",
+	},
+	{
+		"name": "Jane",
+		"type": "🌜",
+		"system": "pc",
+		"game": "TWD Season two"
+	},
+	{
+		"name": "Princess Daphne",
+		"type": "💭",
+		"system": "pc",
+		"game": "Dragons Lair",
+	},
+	{
+		"name": "Maiden in Red",
+		"type": "🌞",
+		"system": "pc",
+		"game": "Betrayer",
+	},
+	{
+		"name": "Anya Oliwa",
+		"type": "👌",
+		"system": "pc",
+		"game": "Wolfenstein the new oreder"
+	},
+	{
+		"name": "Lithariel ",
+		"type": "🌞",
+		"system": "pc",
+		"game": "Shadow of Mordor"	
+	},
+	{
+		"name": "Amita",
+		"type": "💭",
+		"system": "pc",
+		"game": "FarCry 4",
+	},
+	{
+		"name": "Khai Minh Dao",
+		"type": "🌞",
+		"system": "pc",
+		"game": "BF Hardline",
+	},
+	{
+		"name": "Annette Krause",
+		"type": "👌",
+		"system": "pc",
+		"game": "Wolfenstein the old blood"
+	},
+	{
+		"name": "Yennefer",
+		"type": "👌",
+		"system": "pc",
+		"game": "The Witcher 3"
+	},
+	{
+		"name": "Batgirl",
+		"type": "🌞",
+		"system": "pc",
+		"game": "Batman Arkham Knight",
+	},
+	{
+		"name": "Samantha Giddings",
+		"type": "💧",
+		"system":"ps4",
+		"game": "Until Dawn",
+	},
+	{
+		"name": "Hope",
+		"type": "♿",
+		"system": "pc",
+		"game": "Mad Max"
+	},
+	{
+		"name": "Piper Wright",
+		"type": "💭",
+		"system": "pc",
+		"game": "Fallout 4",
+	},
+	{
+		"name": "Lara Croft",
+		"type": "🌜",
+		"system": "pc",
+		"game": "Rise of the Tomb Raider"
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "Layers of fear",
+	},
+	{
+		"name": "Michonne",
+		"type": "🌜",
+		"system": "pc",
+		"game": "TWD Michone"
+	},
+	{
+		"name": "Faye Lau",
+		"type": "🍌",
+		"system": "pc",
+		"game": "The Division"
+	},
+	{
+		"name": "Nadine Ross",
+		"type": "👉",
+		"system":"ps4",
+		"game": "Uncharted 4",
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "DOOM",
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "Inside",
+	},
+	{
+		"name": "Roxy Laveau",
+		"type": "♿",
+		"system": "pc",
+		"game": "Mafia III"
+	},
+	{
+		"name": "Zara Ghufran",
+		"type": "🌞",
+		"system": "pc",
+		"game": "BF 1",
+	},
+	{
+		"name": "Kate García",
+		"type": "🌜",
+		"system": "pc",
+		"game": "TWD A new frontier"
+	},
+	{
+		"name": "Max Caulfield",
+		"type": "💧",
+		"system": "ps4",
+		"game": "Life is Strange"	
+	},
+	{
+		"name": "Vicki Vale",
+		"type": "🌞",
+		"system": "ps4",
+		"game": "Batman The Telltale Series Season 1"	
+	},
+	{
+		"name": "Aloy",
+		"game": "Horizon Zero Dawn",
+		"type": "💧",
+		"system": "ps4",
+	},
+	{
+		"name": "La Santera",
+		"type": "🍌",
+		"system": "pc",
+		"game": "Ghost Reacon"
+	},
+	{
+		"name": "Monika",
+		"type": "📺",
+		"system": "pc",
+		"game": "DokiDoki Literature Club",
+	},	
+	{
+		"name": "Cassandra",
+		"type": "🚶",
+		"system": "pc",
+		"game": "Orwell",
+	},
+	{
+		"name": "Grace Walker",
+		"type": "👌",
+		"system": "pc",
+		"game": "Wolfenstein II the new colossus"
+	},
+	{
+		"name": "Theresa",
+		"type": "🚶",
+		"system": "pc",
+		"game": "Kingdom Come Deliverance",
+	},	
+	{
+		"name": "Faith Seed",
+		"type": "🚶",
+		"system": "pc",
+		"game": "FarCry 5",
+	},
+	{
+		"name": "Freya",
+		"type": "👉",
+		"system":"ps4",
+		"game": "God of War",
+	},
+	{
+		"name": "Kara",
+		"type": "💧",
+		"system":"ps4",
+		"game": "Detroit Become Human",
+	},
+	{
+		"name": "Violet",
+		"type": "🌜",
+		"system": "pc",
+		"game": "TWD final season"
+	},
+	{
+		"name": "Mary Jane",
+		"type": "👉",
+		"system":"ps4",
+		"game": "Spider-man",
+	},	
+	{
+		"name": "Unuratu",
+		"type": "🌜",
+		"system": "pc",
+		"game": "Shadow of the Tomb Raider"
+	},	
+	{
+		"name": "Sadie Adler",
+		"type": "👉",
+		"system":"ps4",
+		"game": "RDR 2",
+	},
+	{
+		"name": "Solveig Fia Bjørnstad",
+		"type": "🌞",
+		"system": "pc",
+		"game": "BF V",
+	},
+	{
+		"name": "Fury",
+		"type": "💧",
+		"system":"ps4",
+		"game": "Darksiders 3",
+	},
+	{
+		"name": "Sarah Whitaker",
+		"type": "👉",
+		"system":"ps4",
+		"game": "Days Gone",
+	},
+	{
+		"name": "Parvati",
+		"type": "♿",
+		"system": "pc",
+		"game": "The Outer Worlds"	
+	},
+	{
+		"name": "Lydia Jorjadze",
+		"type": "🌜",
+		"system": "pc",
+		"game": "SGW 3"
+	},
+	{
+		"name": "Giul",
+		"type": "🛀",
+		"system": "pc",
+		"game": "Metro 2033 Exodus"
+	},
+	{
+		"name": "Valorie",
+		"type": "🚶",
+		"system": "pc",
+		"game": "Minecraft Dungeons"	
+	},
+	{
+		"name": "Second Sister",
+		"type": "🌜",
+		"system": "pc",
+		"game": "Jedi fallen order"
+	},
+	{
+		"name": "Mia Winters",
+		"type": "🍌",
+		"system": "pc",
+		"game": "Resident evil 7"	
+	},
+	{
+		"name": "Antanasia X.Alikhanova",
+		"type": "🌜",
+		"system": "pc",
+		"game": "SGW Contracts"
+	},
+	{
+		"name": "Ellie Williams",
+		"type": "👉",
+		"system":"ps4",
+		"game": "The Last of Us 2",
+	},
+	{
+		"name": "Yuna",
+		"type": "👉",
+		"system":"ps4",
+		"game": "Ghost of Tsushima",
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "DOOM Etharnal",
+	},
+	{
+		"name": "Cerberus",
+		"type": "♿",
+		"system": "pc",
+		"game": "Helltaker"	
+	},
+	{
+		"name": "Judy Álvarez",
+		"type": "📺",
+		"system": "pc",
+		"game": "Cyberpunk 2077",
+	},
+	{
+		"name": "Maya Torres",
+		"type": "♿",
+		"system": "pc",
+		"game": "State of Decay 2"	
+	},
+	{
+		"name": "Leah",
+		"type": "🛀",
+		"system": "pc",
+		"game": "Stardew Valley"	
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "Subnautica"	
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "The Pedestrian"	
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "Carrion"	
+	},
+	{
+		"name": "Catwoman",
+		"type": "🌞",
+		"system": "pc",
+		"game": "Batman Arkham Origins Blackgate",
+	},
+	{
+		"name": "Josephine Aubrac",
+		"type": "🍌",
+		"system": "pc",
+		"game": "Enemy Front"
+	},
+	{
+		"name": "Zoey",
+		"type": "💭",
+		"system": "pc",
+		"game": "Left 4 Dead"
+	},
+	{
+		"name": "Asuka Kasen",
+		"type": "🚶",
+		"system": "pc",
+		"game": "GTA 3"
+	},
+	{
+		"name": "Chun-li",
+		"type": "👉",
+		"system": "snes",
+		"game": "Street Fighter Alpha 2"	
+	},
+	{
+		"name": "Iden Versio",
+		"type": "🛀",
+		"system": "pc",
+		"game": "Battlefront 2"	
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "STALKER"	
+	},
+	{
+		"name": "Jesse",
+		"type": "🛀",
+		"system": "pc",
+		"game": "Control"	
+	},
+	{
+		"name": "",
+		"type": "💀",
+		"system": "pc",
+		"game": "Spec Ops The Line"	
+	},
+	{
+		"name": "Xian Mei",
+		"type": "🌜",
+		"system": "pc",
+		"game": "Dead Island"	
+	},
+	{
+		"name": "Alice Wake",
+		"type": "👌",
+		"system": "pc",
+		"game": "Alan Wake"	
+	},
+	// {
+	// 	"name": "Nisha",
+	// 	"type": "🍌",
+	// 	"system": "pc",
+	// 	"game": "Borderlands Pre-sequel"	
+	// },
+);
+
+
+
+
+//💀👈👉💧🌞📺💭🚶♿🛀🌜👌🍌
